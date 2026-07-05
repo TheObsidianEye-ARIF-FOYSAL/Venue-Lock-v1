a74456696ff7ea33dd23989abdcc66d3a49b857e
TheObsidianEye-ARIF-FOYSAL <ariffoysalruetcse21@gmail.com>
2026-07-05 17:25:49 +0600

    media query 4

--- app/lib/features/volunteer/volunteer_join_screen.dart
+++ app/lib/features/volunteer/volunteer_join_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import '../../app/responsive.dart';
 import '../../app/theme.dart';
 import '../../core/services/student_profile_service.dart';
 import '../../core/services/volunteer_service.dart';
